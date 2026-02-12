27fead98a75b1e0b8fca0be48619f0a54d1a6f8c
Chee Wei <80091094+c-wei20@users.noreply.github.com>
2026-02-12 09:52:25 +0800

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -18,15 +18,15 @@ st.set_page_config(
 # Load the renewable energy data
 @st.cache_data  # Enable caching for improved performance
 def load_data():
-    hydro_data = pd.read_csv('.\dataset\hydro_daily_production_dataset.csv')
+    hydro_data = pd.read_csv('./dataset/hydro_daily_production_dataset.csv')
     hydro_data['Energy Source'] = 'Hydro'
     hydro_data['Date'] = pd.to_datetime(hydro_data['Date'])
 
-    wind_data = pd.read_csv('.\dataset\wind_daily_production_dataset.csv')
+    wind_data = pd.read_csv('./dataset/wind_daily_production_dataset.csv')
     wind_data['Energy Source'] = 'Wind'
     wind_data['Date'] = pd.to_datetime(wind_data['Date'])
 
-    solar_data = pd.read_csv('.\dataset\solar_daily_production_dataset.csv')
+    solar_data = pd.read_csv('./dataset/solar_daily_production_dataset.csv')
     solar_data['Energy Source'] = 'Solar'
     solar_data['Date'] = pd.to_datetime(solar_data['Date'])
 
